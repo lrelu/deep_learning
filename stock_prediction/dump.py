3bde8d9f2c63a19870ec90d8347da8f881367d7f
lrelu <newlik@naver.com>
2018-11-01 16:53:13 +0900

    : Make a code frame for Stock prediction
    # 1. data from cvs
    
    # 2. split data for training and evaluation
    
    # 3. normalize data
    
    # 4. prepare variable
    
    # 5. make a hypothesis
    
    # 6. calc cost
    
    # 7. setup Gradient Descent optimizer
    
    # 8. make session and initialize
    
    # 9. train
    
    # 10. make a model
    
    # 11. evaluation model
    
    # 12. visualize

--- stock_prediction/dump.py
+++ stock_prediction/dump.py
@@ -1,3 +1,26 @@
-import numpy;
-import pandas;
-import matplotlib;
+# make a code frame
+
+# 1. data from cvs
+
+# 2. split data for training and evaluation
+
+# 3. normalize data
+
+# 4. prepare variable
+
+# 5. make a hypothesis
+
+# 6. calc cost
+
+# 7. setup Gradient Descent optimizer
+
+# 8. make session and initialize
+
+# 9. train
+
+# 10. make a model
+
+# 11. evaluation model
+
+# 12. visualize
+
