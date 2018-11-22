9b319059568b34fe7d79fcc4d0140e0e4a24c2be
lrelu <newlik@naver.com>
2018-11-22 17:31:38 +0900

    : release - Send to Dr. Woo

--- vision_processing/TestProject/Frm_DetectFault.Designer.cs
+++ vision_processing/TestProject/Frm_DetectFault.Designer.cs
@@ -37,8 +37,20 @@
 			this.mnuVision = new System.Windows.Forms.ToolStripMenuItem();
 			this.mnuVisionOpen = new System.Windows.Forms.ToolStripMenuItem();
 			this.mnuVisionPause = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuStartImageprocessing = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
 			this.mnuVisionClose = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuMore = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuCapture = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuLoadImage = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+			this.mnuBinarization = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuGetContour = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuOCR = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuGetLanes = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuDeepLearning = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuImageProcessing = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuCnn = new System.Windows.Forms.ToolStripMenuItem();
 			this.pnlMain = new System.Windows.Forms.Panel();
 			this.splMain = new System.Windows.Forms.SplitContainer();
 			this.tblMain = new System.Windows.Forms.TableLayoutPanel();
@@ -51,20 +63,22 @@
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
 			this.imgbGrayscale = new Emgu.CV.UI.ImageBox();
 			this.groupBox5 = new System.Windows.Forms.GroupBox();
+			this.lblAlarm = new System.Windows.Forms.Label();
 			this.rtxtResult = new System.Windows.Forms.RichTextBox();
 			this.imgbResult = new Emgu.CV.UI.ImageBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.trkArea = new System.Windows.Forms.TrackBar();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label1 = new System.Windows.Forms.Label();
 			this.trkThreshLinking = new System.Windows.Forms.TrackBar();
 			this.trkThresh = new System.Windows.Forms.TrackBar();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.label6 = new System.Windows.Forms.Label();
-			this.label7 = new System.Windows.Forms.Label();
-			this.label8 = new System.Windows.Forms.Label();
-			this.label9 = new System.Windows.Forms.Label();
+			this.mnuBinarywithGray = new System.Windows.Forms.ToolStripMenuItem();
 			this.mnuMain.SuspendLayout();
 			this.pnlMain.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.splMain)).BeginInit();
@@ -92,7 +106,9 @@
 			this.mnuMain.ImageScalingSize = new System.Drawing.Size(20, 20);
 			this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuFile,
-            this.mnuVision});
+            this.mnuVision,
+            this.mnuMore,
+            this.mnuDeepLearning});
 			this.mnuMain.Location = new System.Drawing.Point(0, 0);
 			this.mnuMain.Name = "mnuMain";
 			this.mnuMain.Size = new System.Drawing.Size(1262, 28);
@@ -112,18 +128,18 @@
 			// mnuSave
 			// 
 			this.mnuSave.Name = "mnuSave";
-			this.mnuSave.Size = new System.Drawing.Size(216, 26);
+			this.mnuSave.Size = new System.Drawing.Size(114, 26);
 			this.mnuSave.Text = "저장";
 			// 
 			// toolStripMenuItem1
 			// 
 			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-			this.toolStripMenuItem1.Size = new System.Drawing.Size(213, 6);
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(111, 6);
 			// 
 			// mnuClose
 			// 
 			this.mnuClose.Name = "mnuClose";
-			this.mnuClose.Size = new System.Drawing.Size(216, 26);
+			this.mnuClose.Size = new System.Drawing.Size(114, 26);
 			this.mnuClose.Text = "닫기";
 			this.mnuClose.Click += new System.EventHandler(this.mnuClose_Click);
 			// 
@@ -132,6 +148,7 @@
 			this.mnuVision.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuVisionOpen,
             this.mnuVisionPause,
+            this.mnuStartImageprocessing,
             this.toolStripMenuItem2,
             this.mnuVisionClose});
 			this.mnuVision.Name = "mnuVision";
@@ -147,9 +164,18 @@
 			// 
 			// mnuVisionPause
 			// 
+			this.mnuVisionPause.Enabled = false;
 			this.mnuVisionPause.Name = "mnuVisionPause";
 			this.mnuVisionPause.Size = new System.Drawing.Size(114, 26);
 			this.mnuVisionPause.Text = "멈춤";
+			// 
+			// mnuStartImageprocessing
+			// 
+			this.mnuStartImageprocessing.Enabled = false;
+			this.mnuStartImageprocessing.Name = "mnuStartImageprocessing";
+			this.mnuStartImageprocessing.Size = new System.Drawing.Size(114, 26);
+			this.mnuStartImageprocessing.Text = "분석";
+			this.mnuStartImageprocessing.Click += new System.EventHandler(this.mnuStartImageprocessing_Click);
 			// 
 			// toolStripMenuItem2
 			// 
@@ -158,9 +184,100 @@
 			// 
 			// mnuVisionClose
 			// 
+			this.mnuVisionClose.Enabled = false;
 			this.mnuVisionClose.Name = "mnuVisionClose";
 			this.mnuVisionClose.Size = new System.Drawing.Size(114, 26);
 			this.mnuVisionClose.Text = "닫기";
+			// 
+			// mnuMore
+			// 
+			this.mnuMore.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuCapture,
+            this.mnuLoadImage,
+            this.toolStripMenuItem3,
+            this.mnuBinarization,
+            this.mnuBinarywithGray,
+            this.mnuGetContour,
+            this.mnuOCR,
+            this.mnuGetLanes});
+			this.mnuMore.Name = "mnuMore";
+			this.mnuMore.Size = new System.Drawing.Size(81, 24);
+			this.mnuMore.Text = "추가분석";
+			// 
+			// mnuCapture
+			// 
+			this.mnuCapture.Name = "mnuCapture";
+			this.mnuCapture.Size = new System.Drawing.Size(216, 26);
+			this.mnuCapture.Text = "캡쳐";
+			this.mnuCapture.Click += new System.EventHandler(this.mnuCapture_Click);
+			// 
+			// mnuLoadImage
+			// 
+			this.mnuLoadImage.Name = "mnuLoadImage";
+			this.mnuLoadImage.Size = new System.Drawing.Size(216, 26);
+			this.mnuLoadImage.Text = "이미지불러오기";
+			this.mnuLoadImage.Click += new System.EventHandler(this.mnuLoadImage_Click);
+			// 
+			// toolStripMenuItem3
+			// 
+			this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+			this.toolStripMenuItem3.Size = new System.Drawing.Size(213, 6);
+			// 
+			// mnuBinarization
+			// 
+			this.mnuBinarization.Enabled = false;
+			this.mnuBinarization.Name = "mnuBinarization";
+			this.mnuBinarization.Size = new System.Drawing.Size(216, 26);
+			this.mnuBinarization.Text = "이진화";
+			this.mnuBinarization.Click += new System.EventHandler(this.mnuBinarization_Click);
+			// 
+			// mnuGetContour
+			// 
+			this.mnuGetContour.Enabled = false;
+			this.mnuGetContour.Name = "mnuGetContour";
+			this.mnuGetContour.Size = new System.Drawing.Size(216, 26);
+			this.mnuGetContour.Text = "형태검출";
+			this.mnuGetContour.Click += new System.EventHandler(this.mnuGetContour_Click);
+			// 
+			// mnuOCR
+			// 
+			this.mnuOCR.Enabled = false;
+			this.mnuOCR.Name = "mnuOCR";
+			this.mnuOCR.Size = new System.Drawing.Size(216, 26);
+			this.mnuOCR.Text = "OCR";
+			this.mnuOCR.Click += new System.EventHandler(this.mnuOCR_Click);
+			// 
+			// mnuGetLanes
+			// 
+			this.mnuGetLanes.Enabled = false;
+			this.mnuGetLanes.Name = "mnuGetLanes";
+			this.mnuGetLanes.Size = new System.Drawing.Size(216, 26);
+			this.mnuGetLanes.Text = "차선검출";
+			// 
+			// mnuDeepLearning
+			// 
+			this.mnuDeepLearning.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuImageProcessing,
+            this.mnuCnn});
+			this.mnuDeepLearning.Name = "mnuDeepLearning";
+			this.mnuDeepLearning.Size = new System.Drawing.Size(51, 24);
+			this.mnuDeepLearning.Text = "모드";
+			// 
+			// mnuImageProcessing
+			// 
+			this.mnuImageProcessing.Checked = true;
+			this.mnuImageProcessing.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.mnuImageProcessing.Name = "mnuImageProcessing";
+			this.mnuImageProcessing.Size = new System.Drawing.Size(194, 26);
+			this.mnuImageProcessing.Text = "이미지 프로세싱";
+			this.mnuImageProcessing.Click += new System.EventHandler(this.mnuImageProcessing_Click);
+			// 
+			// mnuCnn
+			// 
+			this.mnuCnn.Name = "mnuCnn";
+			this.mnuCnn.Size = new System.Drawing.Size(194, 26);
+			this.mnuCnn.Text = "딥러닝 - CNN";
+			this.mnuCnn.Click += new System.EventHandler(this.mnuCnn_Click);
 			// 
 			// pnlMain
 			// 
@@ -308,6 +425,7 @@
 			// 
 			// groupBox5
 			// 
+			this.groupBox5.Controls.Add(this.lblAlarm);
 			this.groupBox5.Controls.Add(this.rtxtResult);
 			this.groupBox5.Controls.Add(this.imgbResult);
 			this.groupBox5.Controls.Add(this.label9);
@@ -322,6 +440,18 @@
 			this.groupBox5.TabIndex = 9;
 			this.groupBox5.TabStop = false;
 			this.groupBox5.Text = "영상 분석 결과";
+			// 
+			// lblAlarm
+			// 
+			this.lblAlarm.AutoSize = true;
+			this.lblAlarm.Font = new System.Drawing.Font("나눔고딕", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.lblAlarm.ForeColor = System.Drawing.Color.Red;
+			this.lblAlarm.Location = new System.Drawing.Point(568, 222);
+			this.lblAlarm.Name = "lblAlarm";
+			this.lblAlarm.Size = new System.Drawing.Size(307, 55);
+			this.lblAlarm.TabIndex = 2;
+			this.lblAlarm.Text = "동그라미 검출";
+			this.lblAlarm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// rtxtResult
 			// 
@@ -342,6 +472,66 @@
 			this.imgbResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.imgbResult.TabIndex = 5;
 			this.imgbResult.TabStop = false;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label9.Location = new System.Drawing.Point(648, 109);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(19, 20);
+			this.label9.TabIndex = 5;
+			this.label9.Text = "0";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label8.Location = new System.Drawing.Point(648, 73);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(19, 20);
+			this.label8.TabIndex = 5;
+			this.label8.Text = "0";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label6.Location = new System.Drawing.Point(583, 109);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(59, 20);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "Lot X: ";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label7.Location = new System.Drawing.Point(648, 37);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(19, 20);
+			this.label7.TabIndex = 5;
+			this.label7.Text = "0";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label5.Location = new System.Drawing.Point(583, 73);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(64, 20);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Lot ㅁ: ";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label4.Location = new System.Drawing.Point(583, 37);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(61, 20);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Lot O: ";
 			// 
 			// label3
 			// 
@@ -403,65 +593,13 @@
 			this.trkThresh.TabIndex = 3;
 			this.trkThresh.TickFrequency = 10;
 			// 
-			// label4
+			// mnuBinarywithGray
 			// 
-			this.label4.AutoSize = true;
-			this.label4.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.label4.Location = new System.Drawing.Point(583, 37);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(60, 20);
-			this.label4.TabIndex = 5;
-			this.label4.Text = "Lot A: ";
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.label5.Location = new System.Drawing.Point(583, 73);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(58, 20);
-			this.label5.TabIndex = 5;
-			this.label5.Text = "Lot B: ";
-			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.label6.Location = new System.Drawing.Point(583, 109);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(59, 20);
-			this.label6.TabIndex = 5;
-			this.label6.Text = "Lot C: ";
-			// 
-			// label7
-			// 
-			this.label7.AutoSize = true;
-			this.label7.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.label7.Location = new System.Drawing.Point(648, 37);
-			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(19, 20);
-			this.label7.TabIndex = 5;
-			this.label7.Text = "0";
-			// 
-			// label8
-			// 
-			this.label8.AutoSize = true;
-			this.label8.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.label8.Location = new System.Drawing.Point(648, 73);
-			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(19, 20);
-			this.label8.TabIndex = 5;
-			this.label8.Text = "0";
-			// 
-			// label9
-			// 
-			this.label9.AutoSize = true;
-			this.label9.Font = new System.Drawing.Font("나눔고딕", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.label9.Location = new System.Drawing.Point(648, 109);
-			this.label9.Name = "label9";
-			this.label9.Size = new System.Drawing.Size(19, 20);
-			this.label9.TabIndex = 5;
-			this.label9.Text = "0";
+			this.mnuBinarywithGray.Enabled = false;
+			this.mnuBinarywithGray.Name = "mnuBinarywithGray";
+			this.mnuBinarywithGray.Size = new System.Drawing.Size(216, 26);
+			this.mnuBinarywithGray.Text = "이진화 + GrayScale";
+			this.mnuBinarywithGray.Click += new System.EventHandler(this.mnuBinarywithGray_Click);
 			// 
 			// Frm_DetectFault
 			// 
@@ -542,5 +680,19 @@
 		private System.Windows.Forms.Label label7;
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ToolStripMenuItem mnuDeepLearning;
+		private System.Windows.Forms.ToolStripMenuItem mnuCnn;
+		private System.Windows.Forms.ToolStripMenuItem mnuMore;
+		private System.Windows.Forms.ToolStripMenuItem mnuOCR;
+		private System.Windows.Forms.ToolStripMenuItem mnuCapture;
+		private System.Windows.Forms.ToolStripMenuItem mnuGetLanes;
+		private System.Windows.Forms.ToolStripMenuItem mnuImageProcessing;
+		private System.Windows.Forms.ToolStripMenuItem mnuStartImageprocessing;
+		private System.Windows.Forms.Label lblAlarm;
+		private System.Windows.Forms.ToolStripMenuItem mnuLoadImage;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+		private System.Windows.Forms.ToolStripMenuItem mnuGetContour;
+		private System.Windows.Forms.ToolStripMenuItem mnuBinarization;
+		private System.Windows.Forms.ToolStripMenuItem mnuBinarywithGray;
 	}
 }
--- vision_processing/TestProject/Frm_DetectFault.cs
+++ vision_processing/TestProject/Frm_DetectFault.cs
@@ -12,6 +12,7 @@ namespace DetectFault
 	{
 		private Capture _cam = null;
 		private Image<Bgr, Byte> _frame;
+		private Image<Bgr, Byte> _file;
 
 		private Timer _imageProcessor;
 
@@ -35,6 +36,8 @@ namespace DetectFault
 			trkThresh.Value = 180;
 			trkThreshLinking.Value = 120;
 			trkArea.Value = 300;
+
+			lblAlarm.Text = string.Empty;
 		}
 
 		/// <summary>
@@ -44,6 +47,14 @@ namespace DetectFault
 		/// <param name="e"></param>
 		private void ImageProcessing(object sender, EventArgs e)
 		{
+			// 영상 분석이 체크되어 있어야만 분석 시작
+			if (mnuStartImageprocessing.Checked == false)
+				return;
+
+			// 결과 초기화
+			lblAlarm.Text = string.Empty;
+			_imageProcessor.Interval = 1000;
+
 			// 텍스트 추출하려고 했지만, 속도가 받추어지지 않아서 우선 기능 제거
 			// 차후에 처리해야함: 영상 이미지는 뒤집어서 읽기 때문에 영문자가 틀린 부분을 보정해줘야함
 			//rtxtResult.Text = OCR.GetTextfromImage(_frame.Bitmap);
@@ -53,21 +64,18 @@ namespace DetectFault
 
 			// 사각형 그리기
 			foreach (MCvBox2D rectangle in rectangles)
-			{
 				_frame.Draw(rectangle, new Bgr(Color.Blue), 3);
-				Bitmap bitmap = new Bitmap((int)rectangle.size.Width, (int)rectangle.size.Height);
-				using (Graphics g = Graphics.FromImage(bitmap))
-				{
-					
-				}
-			}
 
 			// 원 추출
 			CircleF[][] circles = ImageProcessor.GetCircles(_frame, trkThresh.Value, 180, 5);
 
 			foreach (CircleF circle in circles[0])
-			{
 				_frame.Draw(circle, new Bgr(Color.Red), 3);
+
+			if (circles[0].Length > 0)
+			{
+				lblAlarm.Text = "동그라미 검출";
+				_imageProcessor.Interval = 3000;
 			}
 
 			// 이미지 결과창에 노출
@@ -120,11 +128,132 @@ namespace DetectFault
 
 			// 영상 분석 시작
 			_imageProcessor.Enabled = true;
+
+			// 버튼 활성화
+			mnuVisionPause.Enabled = true;
+			mnuVisionClose.Enabled = true;
+			mnuStartImageprocessing.Enabled = true;
+			mnuVisionOpen.Enabled = false;
 		}
 
+		private void mnuLoadImage_Click(object sender, EventArgs e)
+		{
+			using (OpenFileDialog dialog = new OpenFileDialog())
+			{
+				dialog.InitialDirectory = @"C:\Temp\Images";
+
+				if (dialog.ShowDialog() == DialogResult.OK)
+				{
+					_file = new Image<Bgr, Byte>(new Bitmap(dialog.FileName));
+					imgbResult.Image = _file;
+
+					// 버튼 활성화
+					mnuGetContour.Enabled = true;
+					mnuGetLanes.Enabled = true;
+					mnuOCR.Enabled = true;
+					mnuBinarization.Enabled = true;
+					mnuBinarywithGray.Enabled = true;
+				}
+			}
+		}
+
+		private void mnuGetContour_Click(object sender, EventArgs e)
+		{
+			// 원본사진을 유지하기 위해서 저장
+			Image<Bgr, Byte> image = _file.Clone();
+
+			// 사각형 추출
+			List<MCvBox2D> rectangles = ImageProcessor.GetRectangles(image, trkThresh.Value, trkThreshLinking.Value, trkArea.Value);
+
+			// 사각형 그리기
+			foreach (MCvBox2D rectangle in rectangles)
+				image.Draw(rectangle, new Bgr(Color.Blue), 3);
+
+			// 결과 출력
+			imgbResult.Image = image;
+		}
+
+		private void mnuOCR_Click(object sender, EventArgs e)
+		{
+			// 텍스트 추출
+			Cursor.Current = Cursors.WaitCursor;
+			rtxtResult.Text = OCR.GetTextfromImage(_file.Bitmap);
+			Cursor.Current = Cursors.Default;
+		}
+
+		private void mnuImageProcessing_Click(object sender, EventArgs e)
+		{
+			// 메뉴 스트립 처리
+			if (mnuImageProcessing.Checked)
+				return;
+
+			mnuImageProcessing.Checked = !mnuImageProcessing.Checked;
+			mnuCnn.Checked = !mnuCnn.Checked;
+		}
+
+		private void mnuCnn_Click(object sender, EventArgs e)
+		{
+			// 메뉴 스트립 처리
+			if (mnuCnn.Checked)
+				return;
+
+			mnuCnn.Checked = !mnuCnn.Checked;
+			mnuImageProcessing.Checked = !mnuImageProcessing.Checked;
+		}
+
+		private void mnuBinarization_Click(object sender, EventArgs e)
+		{
+			Cursor.Current = Cursors.WaitCursor;
+			// 배운 내용을 복습하기 위한 Raw데이터를 하나씩 꺼내어 이진화
+			Bitmap inImage = (Bitmap)_file.Bitmap.Clone();
+
+			// 이진화 진행
+			inImage = ImageProcessor.Binarization(inImage, trkThresh.Value);
+
+			// 해당 이미지 출력
+			imgbResult.Image = new Image<Bgr, Byte>(inImage);
+			Cursor.Current = Cursors.Default;
+		}
+  
 		private void mnuClose_Click(object sender, EventArgs e)
 		{
 			this.Close();
+		}
+
+		private void mnuCapture_Click(object sender, EventArgs e)
+		{
+			// 영상 캡쳐
+			imgbResult.Image = _frame;
+
+			// 버튼 활성화
+			mnuGetContour.Enabled = true;
+			mnuGetLanes.Enabled = true;
+			mnuOCR.Enabled = true;
+			mnuBinarization.Enabled = true;
+			mnuBinarywithGray.Enabled = true;
+		}
+
+		private void mnuStartImageprocessing_Click(object sender, EventArgs e)
+		{
+			mnuStartImageprocessing.Checked = !mnuStartImageprocessing.Checked;
+		}
+
+		private void mnuBinarywithGray_Click(object sender, EventArgs e)
+		{
+			Cursor.Current = Cursors.WaitCursor;
+			// 배운 내용을 복습하기 위한 Raw데이터를 하나씩 꺼내어 이진화
+			Bitmap inImage = (Bitmap)_file.Bitmap.Clone();
+
+			// 칼라사진을 바로 이진화 하니까 원하지 않은 결과를 가지고 오기 때문에 그레이 스케일로 변경하였다가, 이진화 진행함
+			double[] threshs = { 0.2989, 0.5870, 0.1140 };
+			inImage = ImageProcessor.Graysacle(inImage, threshs);
+
+			// 이진화 진행
+			inImage = ImageProcessor.Binarization(inImage, trkThresh.Value);
+
+			// 해당 이미지 출력
+			imgbResult.Image = new Image<Bgr, Byte>(inImage);
+			Cursor.Current = Cursors.Default;
 		}
 	}
 }
